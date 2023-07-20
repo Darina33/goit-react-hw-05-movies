ddac4ba486fd206fd54d6906abdada89a044841b
Darina33 <115955389+Darina33@users.noreply.github.com>
2023-07-20 13:45:01 +0300

    Navigate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Shared from './Shared';
 
 const Home = lazy(() => import("../pages/Home/Home"));
@@ -18,7 +18,7 @@ export const App = () => {
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
-        <Route path="*" element={<Home />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Route>
     </Routes>
   );
